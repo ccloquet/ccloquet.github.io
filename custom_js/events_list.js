a8842d7515c99502cdd9c58c803c7019c7cdcba1
Caroline-poppy <61145144+Caroline-poppy@users.noreply.github.com>
2020-02-17 12:32:54 +0100

    Update events_list.js

--- custom_js/events_list.js
+++ custom_js/events_list.js
@@ -1,6 +1,8 @@
 //du 05/06/2019 au 07/06/2019 — Nancy, France                 Conférence INRS 2019-Les risques biologiques
 var events = [
 //['URL', 'TITRE', 'SOUS-TITRE', 'Dates et éventuellement heures', 'Lieu', 'prix', 'Date du jour ou du dernier jour en format YYYY-MM-DD'],
+['https://www.facebook.com/events/816786808837370/830141890835195', '2de Wetenschappelijke Avond rond Evenementen', 'Mass Gathering Solutions', '9 avril 2020 de 18:30 à 22:30', 'Melle (Belgique)', 'gratuit', '2020-04-09'],
+['https://www.psc-europe.eu/', 'Public Safety Communications Conference', 'PSCE Europe (hosted by Belgian Federal Police)', '30 juin-2 juillet 2020', 'Bruxelles', 'TBD', '2020-07-02'],
 ['https://www.cmdrcoe.org/menu.php?m_id=2&c_id=51', 'Disaster Management Course', '', '18-22 mai 2020', 'Sofia - Bulgarie', '200 €', '2020-05-22'],
 ['https://www.cmdrcoe.org/menu.php?m_id=2&c_id=38', 'Strategic Decision Making for Crisis Response Operations Course', '', '23-27 novembre 2020', 'Sofia, Bulgarie', '200 €', '2020-11-27'],
 ['https://www.aerospace-valley.com/agenda/rencontre-g%C3%A9orisque-l%E2%80%99information-geographique-au-service-de-la-gestion-des-risques-et-des', 'Géorisque', ' L\'information géographique au service de la gestion des risques et des crises', '4-5 février 2020', 'Nîmes', 'gratuit', '2020-02-05'],
